4ae39485fe82b18777283129ccbf54eebf620549
Marshall Steele <marshallsteele123@hotmail.com>
2021-06-26 18:28:58 -0400

    Completed lab 5

---
+++ lab05/scripts/main.js
@@ -0,0 +1,220 @@
+function bookAppointment() {
+    var modalEl = document.getElementById('bookingModal');
+    var modal = bootstrap.Modal.getInstance(modalEl);
+
+    if (validateForm()) {
+        modal.hide();
+        alert("Your appointment has been booked. Thank you for chosing Athos Veterinary!");
+    } else {
+        alert("All fields must be completed before booking an appointment. Please complete the form before submitting.");
+    }
+}
+
+/*
+=================================
+VALIDATION METHODS
+=================================
+*/
+
+function validateForm() {
+    return (validatePhone("inputPhoneNumber") 
+        && validateCreditCard("inputCreditCard")
+        && validateRequiredField("inputPetName")
+        && validateRequiredField("inputPetType")
+        && validateRequiredField("inputFirstName")
+        && validateRequiredField("inputLastName")
+        && validateRequiredField("inputEmail"));
+}
+
+function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    var filter = /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+    return filter.test(a);
+}
+
+function validateCreditCard(txtCredit) {
+    var a = document.getElementById(txtCredit).value;
+    var filter = /^\d{4}[\s-]\d{4}[\s-]\d{4}[\s-]\d{4}$/;
+    return filter.test(a);
+}
+
+function validateRequiredField(fieldId) {
+    var a = document.getElementById(fieldId).value;
+    return a.length !== 0;
+}
+
+/*
+=================================
+JQUERY SETUP AND LISTENERS
+=================================
+*/
+
+
+var unavailableDates = [];
+var pradaUnavail = [4, 5];
+var ryanUnavail = [1];
+var rachelUnavail = [3];
+const setDateFormat = "mm/dd/yy";
+
+function disableDates(date) {
+    // Disable weekends and unavailable days
+    if (date.getDay() === 0 || date.getDay() === 6 || unavailableDates.includes(date.getDay()))
+        return [false];
+    else
+        return [true];
+}
+
+
+$(document).ready(function(){
+
+    /*
+    -----------------------------
+    Input validation
+    -----------------------------
+    */
+
+    $("#inputPhoneNumber").on("focusout", function(){
+        var toastEl = document.getElementById('phoneToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validatePhone("inputPhoneNumber")){
+            toast.show();
+            $("#inputPhoneNumber").removeClass("is-valid");
+            $("#inputPhoneNumber").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputPhoneNumber").removeClass("is-invalid");
+            $("#inputPhoneNumber").addClass("is-valid");
+        }
+    });
+
+    $("#inputCreditCard").on("focusout", function(){
+        var toastEl = document.getElementById('creditCardToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validateCreditCard("inputCreditCard")){
+            toast.show();
+            $("#inputCreditCard").removeClass("is-valid");
+            $("#inputCreditCard").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputCreditCard").removeClass("is-invalid");
+            $("#inputCreditCard").addClass("is-valid");
+        }
+    });
+
+    $("#inputPetType").on("focusout", function(){
+        var toastEl = document.getElementById('requiredFieldToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validateRequiredField("inputPetType")){
+            toast.show();
+            $("#inputPetType").removeClass("is-valid");
+            $("#inputPetType").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputPetType").removeClass("is-invalid");
+            $("#inputPetType").addClass("is-valid");
+        }
+    });
+
+    $("#inputPetName").on("focusout", function(){
+        var toastEl = document.getElementById('requiredFieldToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validateRequiredField("inputPetName")){
+            toast.show();
+            $("#inputPetName").removeClass("is-valid");
+            $("#inputPetName").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputPetName").removeClass("is-invalid");
+            $("#inputPetName").addClass("is-valid");
+        }
+    });
+    
+    $("#inputFirstName").on("focusout", function(){
+        var toastEl = document.getElementById('requiredFieldToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validateRequiredField("inputFirstName")){
+            toast.show();
+            $("#inputFirstName").removeClass("is-valid");
+            $("#inputFirstName").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputFirstName").removeClass("is-invalid");
+            $("#inputFirstName").addClass("is-valid");
+        }
+    });
+
+    $("#inputLastName").on("focusout", function(){
+        var toastEl = document.getElementById('requiredFieldToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validateRequiredField("inputLastName")){
+            toast.show();
+            $("#inputLastName").removeClass("is-valid");
+            $("#inputLastName").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputLastName").removeClass("is-invalid");
+            $("#inputLastName").addClass("is-valid");
+        }
+    });
+
+    $("#inputEmail").on("focusout", function(){
+        var toastEl = document.getElementById('requiredFieldToast');
+        var toast = bootstrap.Toast.getInstance(toastEl);
+
+        if (!validateRequiredField("inputEmail")){
+            toast.show();
+            $("#inputEmail").removeClass("is-valid");
+            $("#inputEmail").addClass("is-invalid");
+        }
+        else {
+            toast.hide();
+            $("#inputEmail").removeClass("is-invalid");
+            $("#inputEmail").addClass("is-valid");
+        }
+    });
+
+    /*
+    -----------------------------
+    Date Input Formatting
+    -----------------------------
+    */
+    $("#inputExpert").on("change", function(){
+        switch ($("#inputExpert option:selected").text()) {
+            case "Dr. Jennifer Prada, DVM":
+                unavailableDates = pradaUnavail;
+                break;
+            case "Ryan, RVT":
+                unavailableDates = ryanUnavail;
+                break;
+            case "Rachel, VA":
+                unavailableDates = rachelUnavail;
+                break;
+            default:
+                unavailableDates = [];
+                break;
+        }
+    });
+
+    $( "#dateInput" ).datepicker(
+        {
+            dateFormat: setDateFormat,
+            // no calendar before June 1rst 2020
+            minDate: new Date('06/27/2021'),
+            maxDate: '+4M',
+            // used to disable some dates
+            beforeShowDay: disableDates
+        }
+    );
+});
---
+++ lab05/scripts/service.js
@@ -0,0 +1,71 @@
+var services = {
+    nail: {
+        title: "Nail Trimming",
+        price: "$20.00",
+        description: "Have your pets nails cut."
+    },
+    consult: {
+        title: "Pet Consult",
+        price: "$40.00",
+        description: "If you are concerned about any aspet of your pets health, we will complete a full assessment for you."
+    },
+    spay: {
+        title: "Spay/Neuter",
+        price: "$120.00",
+        description: "An opperation will be performed to say or neuter your animal."
+    },
+    microchip: {
+        title: "Microchipping",
+        price: "$24.00",
+        description: "We never want to lose our pets. Have them microshipped to uniquely identify them should it be needed."
+    },
+    shots: {
+        title: "First Shots",
+        price: "$210.00",
+        description: "It is important for your pets health to ensure they are protected against common diseases. Have them immunized by our capable staff."
+    }
+}
+
+function setDescription(service) {
+    var title = document.getElementById('service_desc_header');
+	title.innerHTML = "";
+    var price = document.getElementById('service_desc_price');
+	price.innerHTML = "";
+    var content = document.getElementById('service_desc_content');
+	content.innerHTML = "";
+
+    var title_text, price_text, content_text;
+
+    switch(service) {
+        case "nail":
+            title_text = services.nail.title;
+            price_text = services.nail.price;
+            content_text = services.nail.description;
+            break;
+        case "consult":
+            title_text = services.consult.title;
+            price_text = services.consult.price;
+            content_text = services.consult.description;
+            break;
+        case "spay":
+            title_text = services.spay.title;
+            price_text = services.spay.price;
+            content_text = services.spay.description;
+            break;
+        case "microchip":
+            title_text = services.microchip.title;
+            price_text = services.microchip.price;
+            content_text = services.microchip.description;
+            break;
+        case "shots":
+            title_text = services.shots.title;
+            price_text = services.shots.price;
+            content_text = services.shots.description;
+            break;
+    }
+
+    title.appendChild(document.createTextNode(title_text));
+    price.appendChild(document.createTextNode(price_text));
+    content.appendChild(document.createTextNode(content_text));
+
+}
